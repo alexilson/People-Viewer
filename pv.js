c9b5e8db531bcd2c2cb015c5569cc3bdf6119b9b
Alex <ilson.alex@gmail.com>
2023-12-10 09:36:09 -0800

    Fixed welcome message, removed commented code

--- pv.js
+++ pv.js
@@ -1,8 +1,7 @@
 const mysql = require("mysql2");
 const queries = require("./helpers/queries.js")
 const inquirer = require("inquirer");
-
-// database stuff
+const figlet = require("figlet");
 
 const db = mysql.createConnection(
     {
@@ -14,53 +13,74 @@ const db = mysql.createConnection(
     console.log(`Connected to the people_db database.`)
   );
 
-// db.query(queries.viewAllDepartments(), (err, results) => {
-//     return console.log(results)
-// });
+// welcome!
 
-// db.query(queries.viewAllEmployees(), (err, results) => {
-//     return console.log(results)
-// });
+function welcome() {
+    welcome_message = ` \nPeople\n Viewer\n `
+    figlet(welcome_message,
+    {
+        font: "Big Money-ne"
+    },
+    function (err, data) {
+        if (err) {
+            return console.dir(err);
+        } else {
+            console.log(data);
+            main_menu();
+        }
+    });
+}
 
-// db.query(queries.addDepartment(), "Holodeck Sanitation", (err, results) => {
-//     return console.table(results)
-// });
-
-// db.query(queries.viewAllDepartments(), (err, results) => {
-//     console.log(results)
-//     return console.table(results)
-// });
-
-// db.query(queries.addRole(), ["Panda Tamer", 12345, 3], (err, results) => {
-//     console.log(results)
-//     return console.table(results)
-// });
-
-// db.query(queries.viewAllRoles(), (err, results) => {
-//     return console.log(results)
-// });
-
-// db.query(queries.addEmployee(), ["Beatrice", "Potts", 0, 3, 1], (err, results) => {
-//     console.log(results)
-//     return console.table(results)
-// });
-
-// db.query(queries.updateEmployeeRole(), [1, 26], (err, results) => {
-//     console.log(results)
-//     return console.table(results)
-// });
-
-// // close connection to db
-// db.end((err) => {
-//     if (err) {
-//         console.error('Error closing connection:', err);
-//     } else {
-//         console.log('Connection closed.');
-//     }
-// });
+// database stuff
 
 
-// inquirer stuff
+// // db.query(queries.viewAllDepartments(), (err, results) => {
+// //     return console.log(results)
+// // });
+
+// // db.query(queries.viewAllEmployees(), (err, results) => {
+// //     return console.log(results)
+// // });
+
+// // db.query(queries.addDepartment(), "Holodeck Sanitation", (err, results) => {
+// //     return console.table(results)
+// // });
+
+// // db.query(queries.viewAllDepartments(), (err, results) => {
+// //     console.log(results)
+// //     return console.table(results)
+// // });
+
+// // db.query(queries.addRole(), ["Panda Tamer", 12345, 3], (err, results) => {
+// //     console.log(results)
+// //     return console.table(results)
+// // });
+
+// // db.query(queries.viewAllRoles(), (err, results) => {
+// //     return console.log(results)
+// // });
+
+// // db.query(queries.addEmployee(), ["Beatrice", "Potts", 0, 3, 1], (err, results) => {
+// //     console.log(results)
+// //     return console.table(results)
+// // });
+
+// // db.query(queries.updateEmployeeRole(), [1, 26], (err, results) => {
+// //     console.log(results)
+// //     return console.table(results)
+// // });
+
+// // // close connection to db
+// // db.end((err) => {
+// //     if (err) {
+// //         console.error('Error closing connection:', err);
+// //     } else {
+// //         console.log('Connection closed.');
+// //     }
+// // });
+
+
+// // inquirer stuff
 
 function main_menu() {
     inquirer
@@ -93,6 +113,10 @@ function main_menu() {
                 {
                     name: 'Update Employee Role',
                     value: 'updateEmployeeRole'
+                },
+                {
+                    name: 'Quit',
+                    value: 'quit'
                 }
             ]
         }
@@ -100,34 +124,6 @@ function main_menu() {
     .then((answers) => {
         console.log(JSON.stringify(answers.choice, null, ' '));
         handle_menu_choice(answers);
-        // switch (answers.choice) {
-        //     case 'viewAllEmployees':
-        //         db.query(queries.viewAllEmployees(), (err, results) => {
-        //             console.table(results)
-        //             return main_menu();
-        //         });
-        //         break;
-            
-        //     case 'viewAllRoles':
-        //         db.query(queries.viewAllRoles(), (err, results) => {
-        //             console.table(results)
-        //             return main_menu();
-        //         });
-        //         break;
-            
-        //     case 'viewAllDepartments':
-        //         db.query(queries.viewAllDepartments(), (err, results) => {
-        //             console.table(results)
-        //             return main_menu();
-        //         });
-        //         break;
-            
-        //     case 'addEmployee':
-        //         // run query for role list
-        //         // run query for manager list
-        //         return add_employee_menu(role_list, manager_list);
-        // }
-        // return main_menu();
     });
 }
 
@@ -156,17 +152,20 @@ function handle_menu_choice (answers) {
         
         case 'addEmployee':
             // run query for role list
-            // run query for manager list
-            return add_employee_menu(role_list, manager_list);
+            // run query for employee list for manager selection
+            return add_employee_menu(role_list, employee_list);
+
+        case 'quit':
+            return process.exit();
     }
-    // return main_menu();
 }
 
-function add_employee_menu() {
+// function add_employee_menu() {
 
-}
+// }
 
-main_menu();
+welcome();
+// main_menu();
 
 // what other inquirer menus will I need?
 
@@ -182,47 +181,3 @@ main_menu();
 // add Department
 
 // update Employee
-
-// inquirer
-//     .prompt([
-//         {
-//             type: 'list',
-//             message: 'Main Menu - Choose an option:\n',
-//             name: 'choice',
-//             choices: [
-//                 {
-//                     name: 'View All Employees',
-//                     value: 'viewAllEmployees'
-//                 },
-//                 {
-//                     name: 'View All Roles',
-//                     value: 'viewAllRoles'
-//                 },
-//                 {
-//                     name: 'View All Departments',
-//                     value: 'viewAllDepartments'
-//                 },
-//                 {
-//                     name: 'Add An Employee',
-//                     value: 'addEmployee'
-//                 },
-//                 {
-//                     name: 'Add A Role',
-//                     value: 'addRole'
-//                 },
-//                 {
-//                     name: 'Update Employee Role',
-//                     value: 'updateEmployeeRole'
-//                 }
-//             ]
-//         }
-//     ])
-//     .then((answers) => {
-//         console.log(JSON.stringify(answers.choice, null, ' '));
-//         switch (answers.choice) {
-//             case 'viewAllDepartments':
-//                 db.query(queries.viewAllDepartments(), (err, results) => {
-//                     return console.log(results)
-//                 });
-//         }
-//     });
